28c2bd8392845d44b5f7dd3f8d354a31ae02383d
Vania <ivan_datsiv@mail.ru>
2017-01-31 00:37:58 +0200

    Configuring webpack

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -2,12 +2,10 @@ import big from '../assets/big.jpg';
 import small from '../assets/small.jpg';
 import '../styles/image_viewer.css';
 
-const image = document.createElement('img');
-image.src = small;
+export default () => {
+    const image = document.createElement('img');
+    image.src = small;
 
-document.body.appendChild(image);
+    document.body.appendChild(image);
 
-const bigImage = document.createElement('img');
-bigImage.src = big;
-
-document.body.appendChild(bigImage);
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import sum from './sum';
-import './image_viewer';
+const button = document.createElement("button");
+button.innerText = "Picture";
 
-const  total = sum(10, 5);
-console.log(total);
+button.onclick = () => {
+    System.import('./image_viewer').then(module => {
+        module.default();
+        console.log("world");
+    });
+}
+
+document.body.appendChild(button);
